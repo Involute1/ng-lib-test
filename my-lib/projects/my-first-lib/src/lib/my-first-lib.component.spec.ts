f28eabbe55109deded7c4eab69ff22addbeb693f
Luis.Nothvogel <L_noth@yahoo.de>
2023-04-14 18:08:46 +0200

    init

---
+++ my-lib/projects/my-first-lib/src/lib/my-first-lib.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyFirstLibComponent } from './my-first-lib.component';
+
+describe('MyFirstLibComponent', () => {
+  let component: MyFirstLibComponent;
+  let fixture: ComponentFixture<MyFirstLibComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MyFirstLibComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MyFirstLibComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
